31bac64dbdee98432980ed5d5f70f95bf10e0bb6
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2021-12-22 15:09:41 +0000

    Complete Task 1

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /* 
 👉 Write your kata here!
+
+//make a function
+//that takes in a number
+//and returns an array of all of it's multiples 
+    //the array is in ascending order
+
+input - 10
+output - [ 1, 2, 5, 10 ]
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+function multiples(number) {
+
+    return number;
+}
