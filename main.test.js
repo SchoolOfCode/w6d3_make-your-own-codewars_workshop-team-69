1afc351ea3f84bf3a8f8e3de1fb5d6af87c10859
zazu <z.biedzinska@gmail.com>
2021-12-22 15:46:58 +0000

    mock test

--- main.test.js
+++ main.test.js
@@ -1,1 +1,38 @@
 //👉 Write your tests below here:
+
+/*// design a test for a function
+//that takes in a number
+//and returns an array of all of it's multiples 
+    //the array is in ascending order
+
+input - 10
+output - [ 1, 2, 5, 10 ]
+
+// check all of the different types of inputs
+// write all tests in a single describe block
+    // check 10 --> [ 1, 2, 5, 10 ]
+    // check 0  --> print out '0 is not divisible'
+    // check -10 --> [ 1, 2, 5, 10 ]
+    // check - 54 --> []
+    // check 100 --> 
+    // check 81
+    // check 262
+expect(). [ 1, 2, 5, 10 ]
+*/
+
+
+
+describe('test multiples func', () => {
+
+    test('check 10 for it multiples in an array in ascending order', () =>{
+
+        const num = 10;
+        const expected = [ 1, 2, 5, 10 ];
+    
+        const actual = multiples(num);
+    
+        expected(actual).toStrictEqual(expected);
+    
+    })
+
+})
